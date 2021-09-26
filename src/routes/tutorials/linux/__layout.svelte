9728bffe942935198844b7c06446520c5ddf9abb
Nathan Hare <me@laspruca.nz>
2021-09-26 22:42:28 +1300

    Begin work on tutorials

---
+++ src/routes/tutorials/linux/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+	import '../../../css/tutorial.scss';
+	import { process } from "highlight-ts";
+
+	let codeElements = document.getElementsByName("code");
+
+	for (let element of codeElements) {
+	}
+</script>
+
+<div class='tutorial'>
+	<slot />
+</div>
